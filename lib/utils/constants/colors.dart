3cee944ebf998e8ff2d4968927dc38deea8a67a9
Sinda <katfaouisinda9@gmail.com>
2026-03-17 02:19:12 +0100

    colors

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -3,32 +3,54 @@ import 'package:flutter/material.dart';
 class TColors {
 
   // App Basic Colors
-  static const Color primary = Color(0xFF4CAF50);
-  static const Color secondary = Color(0xFF121212);
-  static const Color darkGrey = Color(0xFF282828);
-  static const Color grey = Color(0xFF777777);
-  static const Color white = Color(0xFFFFFFFF);
+  static const Color primary      = Color(0xFFC8102E);
+  static const Color secondary    = Color(0xFF1A1A2E);
+  static const Color darkGrey     = Color(0xFF444444);
+  static const Color grey         = Color(0xFF9A9A9A);
+  static const Color white        = Color(0xFFFFFFFF);
 
   // Text Colors
-  static const Color textPrimary = Color(0xFF4CAF50);
-  static const Color textSecondary = Color(0xFF121212);
-  static const Color textDarkGrey = Color(0xFF282828);
-  static const Color textGrey = Color(0xFF777777);
-  static const Color textWhite = Color(0xFFFFFFFF);
+  static const Color textPrimary  = Color(0xFF1A1A1A);
+  static const Color textSecondary= Color(0xFF666666);
+  static const Color textDarkGrey = Color(0xFF444444);
+  static const Color textGrey     = Color(0xFF9A9A9A);
+  static const Color textWhite    = Color(0xFFFFFFFF);
+  static const Color textHint     = Color(0xFF9A9A9A);
 
   // Background Colors
-  static const Color light = Color(0xFFF2EDF4);
-  static const Color dark = Color(0xFF303030);
+  static const Color light        = Color(0xFFF5F4F0);
+  static const Color dark         = Color(0xFF0F0F0F);
 
   // Background Container Colors
   static const Color lightContainer = Color(0xFFEEEEEE);
-  static const Color darkContainer = Color(0xFF9E9E9E);
+  static const Color darkContainer  = Color(0xFF1E1E1E);
+
+  // Card Colors
+  static const Color cardLight    = Color(0xFFFFFFFF);
+  static const Color cardDark     = Color(0xFF1E1E1E);
 
   // Button Colors
-  static const Color buttonPrimary = Color(0xFF4CAF50);
+  static const Color buttonPrimary   = Color(0xFFC8102E);
   static const Color buttonSecondary = Color(0xFFFFFFFF);
-  static const Color buttonDisabled = Color(0xFF9F9F9F);
+  static const Color buttonDisabled  = Color(0xFF9F9F9F);
 
   // Border Colors
-  static const Color lightBorder = Color(0xFF282828);
+  static const Color lightBorder  = Color(0xFFE0E0E0);
+  static const Color borderLight  = Color(0xFFE0E0E0);
+  static const Color borderDark   = Color(0xFF2A2A2A);
+  static const Color black        = Color(0xFF0D0D0D);
+
+  // Status Colors
+  static const Color success      = Color(0xFF2E7D32);
+  static const Color successLight = Color(0xFFE8F5E9);
+  static const Color warning      = Color(0xFFE65100);
+  static const Color warningLight = Color(0xFFFFF3E0);
+  static const Color info         = Color(0xFF1565C0);
+  static const Color infoLight    = Color(0xFFE3F2FD);
+  static const Color error        = Color(0xFFC62828);
+  static const Color errorLight   = Color(0xFFFFEBEE);
+
+  // Primary Shades
+  static const Color primaryLight = Color(0xFFFFE5E8);
+  static const Color primaryDark  = Color(0xFF990020);
 }
