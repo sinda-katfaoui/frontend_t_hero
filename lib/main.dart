89ebac5860567c79149f9ddbcea292148358dec0
Sinda <katfaouisinda9@gmail.com>
2026-03-17 02:19:48 +0100

    main_beforechanges

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:frontend_t_hero/screens/auth/splash_screen.dart';
 import 'package:frontend_t_hero/utils/themes/theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'T Hero — بطل تونس',
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      home: Container(), 
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      home: const SplashScreen(),
     );
   }
 }
-
